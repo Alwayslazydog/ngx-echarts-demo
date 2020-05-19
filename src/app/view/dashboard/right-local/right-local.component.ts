ac65f3d07129e59cd4a7e5f7ba96ce33e206e96d
Alwayslazydog <zhjupiter@sina.com>
2020-05-19 10:25:07 +0800

    update: clean data ; rank style

--- src/app/view/dashboard/right-local/right-local.component.ts
+++ src/app/view/dashboard/right-local/right-local.component.ts
@@ -7,18 +7,7 @@ import { DashboardService } from '../../../api/dashboard.service'
   styleUrls: ['./right-local.component.scss']
 })
 export class RightLocalComponent implements OnInit {
-  dataList = [
-    { name: '深圳', value: '30202', rank: '1' },
-    { name: '广州', value: '27621', rank: '-1' },
-    { name: '汕头', value: '16233', rank: '' },
-    { name: '湛江', value: '8862', rank: '5' },
-    { name: '韶关', value: '5438', rank: '' },
-    { name: '惠州', value: '5322', rank: '6' },
-    { name: '东莞', value: '4266', rank: '' },
-    { name: '珠海', value: '4028', rank: '' },
-    { name: '潮州', value: '4019', rank: '-3' },
-    { name: '河源', value: '3770', rank: '' }
-  ]
+  dataList = []
   constructor(
     public dashboardService: DashboardService
   ) { }
